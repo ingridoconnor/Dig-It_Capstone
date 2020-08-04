4e86d9f14c1b52a3937a814c2c61eaf6a91638f1
Doyle ODonnell <doyleodo@yahoo.com>
2020-08-04 10:19:09 -0400

    Added plant, plantDAO and plantSqlDAO files to Java

---
+++ final-capstone/java/src/main/java/com/techelevator/dao/PlantSqlDAO.java
@@ -0,0 +1,46 @@
+package com.techelevator.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.Plant;
+
+
+public class PlantSqlDAO implements PlantDAO {
+	
+	private JdbcTemplate template;
+
+	public PlantSqlDAO(DataSource datasource) {
+	    template = new JdbcTemplate(datasource);
+	}
+
+
+	@Override
+	public List<Plant> getAllPlants() {
+	    String sqlPuppy = "SELECT * from plant";
+	    SqlRowSet results = template.queryForRowSet(sqlPuppy);
+	    return mapResultsToPlants(results);    
+	}
+	
+	private Plant mapResultToPuppy(SqlRowSet result) {
+	    Plant retrievedPuppy = new Plant(result.getInt("id"), result.getString("name"), result.getString("description"),
+	            result.getInt("plantsPerSqFoot"), result.getString("sunRequirements"), result.getString("region"), result.getBigDecimal("seedCost"));
+
+	    return retrievedPuppy;
+	}
+
+	private List<Plant> mapResultsToPlants(SqlRowSet results) {
+	    List<Plant> retrievedPuppies = new ArrayList<>();
+	    while (results.next()) {
+	        retrievedPuppies.add(mapResultToPuppy(results));
+	    }
+
+	    return retrievedPuppies;
+	}
+	
+}
---
+++ final-capstone/java/src/main/java/com/techelevator/model/Plant.java
@@ -0,0 +1,97 @@
+package com.techelevator.model;
+
+import java.math.BigDecimal;
+
+public class Plant {
+	
+	private int id;
+	private String name;
+	private String description;
+	private int plantsPerSqFoot;
+	private String sunRequirements;
+	private String region;
+	private BigDecimal seedCost;
+
+	public Plant() {
+	}
+	
+	public Plant(String name, String description, int plantsPerSqFoot, String sunRequirements, String region,
+			BigDecimal seedCost) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.plantsPerSqFoot = plantsPerSqFoot;
+		this.sunRequirements = sunRequirements;
+		this.region = region;
+		this.seedCost = seedCost;
+	}
+	
+	public Plant(int id, String name, String description, int plantsPerSqFoot, String sunRequirements, String region,
+			BigDecimal seedCost) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.plantsPerSqFoot = plantsPerSqFoot;
+		this.sunRequirements = sunRequirements;
+		this.region = region;
+		this.seedCost = seedCost;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPlantsPerSqFoot() {
+		return plantsPerSqFoot;
+	}
+
+	public void setPlantsPerSqFoot(int plantsPerSqFoot) {
+		this.plantsPerSqFoot = plantsPerSqFoot;
+	}
+
+	public String getSunRequirements() {
+		return sunRequirements;
+	}
+
+	public void setSunRequirements(String sunRequirements) {
+		this.sunRequirements = sunRequirements;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public BigDecimal getSeedCost() {
+		return seedCost;
+	}
+
+	public void setSeedCost(BigDecimal seedCost) {
+		this.seedCost = seedCost;
+	}
+
+}
