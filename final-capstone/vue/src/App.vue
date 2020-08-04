712d5a6c56db41d7621997bdcad35ca9473afc21
Doyle ODonnell <doyleodo@yahoo.com>
2020-08-04 17:14:17 -0400

    added styling and links to Home

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
+    <img class="logo" src="../public/digit.png" alt="digit logo">
+    <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+    <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-view />
+      <div id="nav">
+
+      </div>
+
+        
+    
   </div>
 </template>
+
+<style>
+.logo {
+  width: 200px;
+}
+
+div {
+  background-color: #c1c56d;
+}
+</style>
--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
+  <div>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <p></p>
   </div>
+
+  <div class="links">
+    <router-link class="link" v-bind:to="{ name: 'login' }" v-if="$store.state.token != ''">
+      Login
+    </router-link>
+    <router-link class="link" v-bind:to="{ name: 'register' }" v-if="$store.state.token != ''">
+      Register
+    </router-link>
+  </div>
+  
+</div>
+  
 </template>
 
 <script>
@@ -10,3 +23,20 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+.home {
+  display: flex;
+  margin: 100px;
+  justify-content: center;
+  background-color: #c1c56d;
+}
+
+.links {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-size: 2em;
+  color: #e48438;
+}
+</style>
