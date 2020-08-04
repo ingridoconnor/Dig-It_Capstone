98b7b208b1e0dffe4ba59b06ff6a96446089309e
Doyle ODonnell <doyleodo@yahoo.com>
2020-08-04 17:26:32 -0400

    added styling and links to login and register

---
+++ final-capstone/vue/src/services/PlantService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const http = axios.create( {
+    baseURL: "http://localhost:8080/api"
+});
+
+export default {
+    getAllPlants() {
+        return http.get("/allPlants");
+    }
+}
--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
   <div class="home">
-    <h1>Home</h1>
-    <p></p>
+    <h1>dig it! is an online gardening app that let's you create a garden!</h1>
+  </div>
+  <div class="home">
+    <h1>Let's get digging!</h1>
   </div>
 
   <div class="links">
@@ -27,7 +29,7 @@ export default {
 <style>
 .home {
   display: flex;
-  margin: 100px;
+  margin: 40px 100px 40px 100px;
   justify-content: center;
   background-color: #c1c56d;
 }
@@ -38,5 +40,6 @@ export default {
   justify-content: space-around;
   font-size: 2em;
   color: #e48438;
+  margin: 40px;
 }
 </style>
